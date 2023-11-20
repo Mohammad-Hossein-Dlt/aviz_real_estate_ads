9df63be2fc3e6dd7c2ea07982284f711be5db0d3
Mohammad_Hosein.Dlt <79458791+Mohammad-Hossein-Dlt@users.noreply.github.com>
2023-11-20 21:00:03 +0330

    Delete lib directory

--- lib/Constants/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color red = Color(0xffFE60023);
-const Color grey = Color(0xffF9FAFB);
-const Color grey2 = Color(0xffEAECF0);
-const Color grey3 = Color(0xff98A2B3);
-const Color grey4 = Color(0xff101828);
